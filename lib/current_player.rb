ad9901a98bbd5054b38f97d2e7b77cc2b10d0eac
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-04-27 02:27:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,8 @@
 def turn_count(board)
   count = 0
   index = 0
-  if (board[index] != " " || board[index] != "")
+board.each do |place|
+  if place == " " || place == ""
     count += 1
   end
 end
